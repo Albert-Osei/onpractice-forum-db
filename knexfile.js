9c056ac47391c7f31c3ddd73678f1222347188e1
Albert Osei <73245719+Albert-Osei@users.noreply.github.com>
2021-06-09 01:41:54 +0000

    rocket: Add production connection for deployment

--- knexfile.js
+++ knexfile.js
@@ -3,4 +3,8 @@ module.exports = {
     client: 'pg',
     connection: 'postgresql://postgres:4245@localhost:5432/community_forum'
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+  }
 };
