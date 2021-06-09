dbbf3730702f9d09fa0f226bcfa5275381ce8053
Albert Osei <73245719+Albert-Osei@users.noreply.github.com>
2021-06-09 01:21:39 +0000

    tada: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Community Forum DB
+
+![](https://lucid.app/publicSegments/view/1e26ba92-a2d7-423f-bd9b-36858ee8fb34/image.png)
---
+++ knexfile.js
@@ -0,0 +1,6 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgresql://postgres:4245@localhost:5432/community_forum'
+  },
+};
